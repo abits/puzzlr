d810daabe722367bf8c12ed5b2ee148a3713615a
Christoph Martel <chris@codeways.org>
2018-11-28 20:42:42 +0100

    wip

--- install_bx.sh
+++ install_bx.sh
@@ -15,6 +15,9 @@ export PATH="/tmp/Bluemix_CLI/bin:$PATH"
 echo "Install the Bluemix container-service plugin"
 bx plugin install container-service -r Bluemix
 
+echo "Setp Bluemix API endpoint"
+bx api https://api.eu-de.bluemix.net
+
 echo "Install kubectl"
 wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x /tmp/Bluemix_CLI/bin/kubectl
